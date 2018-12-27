1b1550c57a35bbfbc5cfc8820d885cf2a5704882
Nicholas Rodine <halfofastaple@gmail.com>
2018-12-27 17:24:24 -0500

    Added lock seperate from door

--- TimeObjects/Door.gd
+++ TimeObjects/Door.gd
@@ -2,6 +2,9 @@ extends "res://TimeObjects/BaseObject.gd"
 
 signal go_to_next_level();
 
+func _process(_delta):
+	$Keyhole.frame = $Sprite.frame;
+
 func unlock():
 	if ($Sprite.frame < 16):
 		emit_signal("go_to_next_level");
