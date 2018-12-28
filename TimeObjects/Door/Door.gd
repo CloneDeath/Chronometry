3b470dbfb8c0da9d1c9408bee1e1355fbcf5dff8
Nicholas Rodine <halfofastaple@gmail.com>
2018-12-28 12:29:33 -0500

    modified locks and doors, to allow two locks for the 2nd stage

--- TimeObjects/Door/Door.gd
+++ TimeObjects/Door/Door.gd
@@ -18,8 +18,44 @@ func SetTime(value):
 		var lock = get_node(path);
 		lock.SetTime(value);
 
+func all_locks_are_open():
+	for path in lock_paths:
+		var lock = get_node(path);
+		if (lock.locked):
+			return false;
+	return true;
+
+func set_lock_visibility(visibility):
+	for path in lock_paths:
+		var lock = get_node(path);
+		lock.visible = visibility;
+
+func _process(_delta):
+	update_door_state();
+
+func update_door_state():
+	if (all_locks_are_open()):
+		set_lock_visibility(false);
+		$DoorOpen.visible = true;
+	else:
+		set_lock_visibility(true);
+		$DoorOpen.visible = false;
+
 func unlock():
-	if ($Sprite.frame < 16):
+	var locked = false;
+	for path in lock_paths:
+		var lock = get_node(path);
+		if (lock.locked):
+			update_door_state();
+			locked = lock.unlock();
+	return locked;
+
+func _on_Area2D_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton \
+    	and event.button_index == BUTTON_LEFT \
+    	and event.is_pressed():
+        self.on_click()
+
+func on_click():
+	if (all_locks_are_open()):
 		emit_signal("go_to_next_level");
-		return true;
-	return false;
--- TimeObjects/Door/Lock/Lock.gd
+++ TimeObjects/Door/Lock/Lock.gd
@@ -6,6 +6,8 @@ var open_time = null;
 func SetTime(value):
 	.SetTime(value);
 	$Key.frame = $Sprite.frame;
+
+func _process(_delta):
 	self.locked = open_time == null || current_time < open_time;
 
 func set_locked(value):
@@ -15,4 +17,7 @@ func get_locked():
 	return !$Key.visible;
 
 func unlock():
-	open_time = current_time;
+	if ($Sprite.frame < 16 && self.locked):
+		open_time = current_time;
+		return true;
+	return false;
